ad8d1f11fad952639a6a63d1b9529accfd6cc054
Deon Fernandes <dferns0322@gmail.com>
2022-05-23 18:09:30 +0530

    Update gui.py
    Gui Trial and Error

--- CODE/gui.py
+++ CODE/gui.py
@@ -1,3 +1,54 @@
-import PySimpleGUI as sg
+from tkinter import *
+import plotly.plotly as py
+import plotly.graph_objs as go 
+import pandas as pd
+from datetime import datetime
+import pandas.io.data as web
 
-sg.Window(title="Data Analysis", layout=[[]], margins=(500, 250)).read()
+mGui = Tk()
+
+mGui.geometry('651x700+51+51')
+mGui.title('Plotly at Tkinter')
+
+df = web.DataReader("AAPL", 'yahoo',
+                    datetime(2007, 10, 1),
+                    datetime(2016, 7, 11))
+
+trace = go.Scatter(x=df.index,
+                   y=df.High)
+
+
+data = [trace]
+layout = dict(
+    title='Time series with range slider and selectors',
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label='1m',
+                     step='month',
+                     stepmode='backward'),
+                dict(count=6,
+                     label='6m',
+                     step='month',
+                     stepmode='backward'),
+                dict(count=1,
+                    label='YTD',
+                    step='year',
+                    stepmode='todate'),
+                dict(count=1,
+                    label='1y',
+                    step='year',
+                    stepmode='backward'),
+                dict(step='all')
+            ])
+        ),
+        rangeslider=dict(),
+        type='date'
+    )
+)
+
+fig = dict(data=data, layout=layout)
+py.iplot(fig)
+
+mGui.mainloop()
